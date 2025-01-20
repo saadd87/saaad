dafdb86b04bdcc81d355c0e5f63e690dac0c3b5f
saadd87 <saadmehmood200@gmail.com>
2025-01-20 13:19:40 +0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,9 @@
+// Function to display an alert
+function showAlert() {
+    alert("Hello! Welcome to my first HTML page.");
+}
+
+// Function to change the content of a paragraph
+function changeContent() {
+    document.getElementById("dynamicContent").innerText = "This content has been changed!";
+}
